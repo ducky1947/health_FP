ae06272eab64aff28d19bc9e0051091b5680b2ac
ducky1947 <kewalraneail@gmail.com>
2025-09-10 17:00:00 +0530

    add basic user authentication and some other

--- djangosite/core/views.py
+++ djangosite/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
 from .forms import ItemForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 def item_list(request):
     items = Item.objects.all()
@@ -37,3 +40,14 @@ def item_delete(request, pk):
         item.delete()
         return redirect('item_list')
     return render(request, 'core/item_delete.html', {'item': item})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('item_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
